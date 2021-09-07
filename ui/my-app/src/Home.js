92a3724ace961bed6dd7ff88844f07a655fa3035
wilgato <wil.kimel@hotmail.com>
2021-09-07 20:19:01 -0300

    projeto-integrador

---
+++ ui/my-app/src/Home.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+export class Home extends Component {
+  render() {
+    return (
+      <div>
+        <h3>Projeto Integrador Sistema Inventario</h3>
+        <h3>CAEW System WEB</h3>
+      </div>
+    );
+  }
+}
